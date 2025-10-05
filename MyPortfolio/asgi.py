0fe49636992f3ca6402531a4193e30a875873fdf
Black-Ace-Gr <gregorydavid373@gmail.com>
2025-10-05 09:05:29 +0300

    ace revolution final deployment

--- MyPortfolio/asgi.py
+++ MyPortfolio/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gregory_otieno.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gregory_otieno_ace.settings')
 
 application = get_asgi_application()
